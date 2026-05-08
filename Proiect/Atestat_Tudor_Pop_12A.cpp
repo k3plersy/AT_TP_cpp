fef382b6d72781c4d237c1b15ad5acc7c2f481b6
k3plersy <pop.tudor.2007@gmail.com>
2026-05-08 14:04:24 +0300

    Add files via upload

--- Proiect/Atestat_Tudor_Pop_12A.cpp
+++ Proiect/Atestat_Tudor_Pop_12A.cpp
@@ -4,27 +4,26 @@ using namespace std;
 ifstream fin("atestat.in");
 ofstream fout("atestat.out");
 
-int n , m , rep[101]; //initializare nr noduri, nr muchii, vectorul reprezentantiilor
-int v[5000];
+void alg_prim()
+{
+    
+}
 
 struct muchie
 {
-    int i,j,cost; 
+    int i,j,cost;
 };
-muchie x[5000]; //initializare vector pentru muchii si costurile acestora ca si var de tip "muchie"
+muchie x[5000]; //initializare vector pentru muchii si costurile acestora
 
-
-int citire(int &n, int &m, muchie x[]) //citirea nr de noduri, nr de muchii si apoi a muchiilor si a costurilor acestora
+void alg_kruskal(muchie x[])
 {
-    fin >> n >> m;
-    for(int i = 0 ; i < m ; i++)
-        fin >> x[i].i >> x[i].j >> x[i].cost;
-    return 0;
-}
+    int rep[101],n,m; //initializare vectorul reprezentantiilor, nr noduri, nr muchii
+    int v[5000]; //vector pt verificarea muchiilor(pt evitarea muchiilor nedorite)
 
-void sortare(int n,int m, muchie x[]) //sortare tablou x[] după campul cost
-{
-    for(int i = 0; i < m-1; i++) 
+    cin >> n >> m;
+    for(int i = 0 ; i < m ; i++) cin >> x[i].i >> x[i].j >> x[i].cost; //citirea nr de noduri, nr de muchii si apoi a muchiilor si a costurilor acestora
+
+    for(int i = 0; i < m-1; i++) //sortare tablou x[] după campul cost
         for(int j = i+1; j < m; j++)
             if(x[i].cost > x[j].cost)
             {
@@ -32,18 +31,11 @@ void sortare(int n,int m, muchie x[]) //sortare tablou x[] după campul cost
                 x[i] = x[j];
                 x[j] = aux;
             }
-}
-
-int main()
-{
-    int cnt = 0; //contor pt verificare
-    citire(n,m,x); 
-    sortare(n,m,x);
 
     for(int i = 1 ; i <= n ; i++) rep[i] = i; //initializare reprezentanti
 
     //determinare APM
-    int S = 0;
+    int S = 0, cnt = 0;
     for(int i = 0 ; i < m && cnt < n ; i++)
         if(rep[x[i].i] != rep[x[i].j]) // extremitatile sunt din subarbori diferiti
         {
@@ -55,9 +47,38 @@ int main()
                 if(rep[j] == aj)
                     rep[j] = ai;
         }
-    fout << S << endl; //afisare cost
+    cout << "cost APM: " << S << endl << "APM(muchii si costurile lor)" << endl; //afisare cost
     for(int i = 0; i < m; i++)
-        if(v[i]==1) //afisare APM + 
-            fout << x[i].i << " " << x[i].j << " " << x[i].cost << endl;
+        if(v[i]==1) //afisare APM + costuri
+            cout << x[i].i << " " << x[i].j << " " << x[i].cost << endl;
+}
+
+int main()
+{
+    int USER, T = 1;
+    
+    while(T != 0)
+    {   
+        cout << "ALGORITMI GRAFURI" << endl << "Ce program doriti sa utilizati?" << endl;
+        cout << "1. Algoritmul lui Kruskal" << endl;
+        cout << "2. " << endl;
+        cout << "3. " << endl;
+        cout << endl << "Introduceti numarul programului: "; cin>>USER;
+
+        if(USER == 1) 
+        {
+            cout << "Introduceti nr de noduri, nr de muchii, si lista muchiilor cu costuri" << endl;
+            alg_kruskal(x);
+        }
+        else if(USER == 2)
+        {
+
+        }
+        else if(USER == 3)
+        {
+
+        }
+        cout << "Doriti sa continuati? 1 / 0" << endl; cin>>T;
+    }
     return 0;
 }
